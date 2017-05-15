eca27afe4975a67ea746cc4e080b86385a336cbe
GorCat <745318341@qq.com>
2017-05-15 10:20:40 +0800

    增加了工程文件

---
+++ Example/AiLiPaymentExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AiLiPaymentExample
+//
+//  Created by GorCat on 2017/5/15.
+//  Copyright © 2017年 GorCat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
